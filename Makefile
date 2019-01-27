c6e414f7e74fc2624d02059cec947b2f27adcbad
EduardTigLar <edutigsel@gmail.com>
2019-01-27 10:51:47 -0500

    archivo README, moidifcacion del Makefile

--- Makefile
+++ Makefile
@@ -8,8 +8,8 @@ all: monitord monitorc
 monitord: $(SERVER_PATH_PREFIX)monitord.c $(SERVER_PATH_PREFIX)helper_functions.c src/csapp.c $(SERVER_PATH_PREFIX)sysinfo.c src/protobuf-c.c src/monitor.pb-c.c
 	gcc -I. -o monitord $(SERVER_PATH_PREFIX)monitord.c $(SERVER_PATH_PREFIX)helper_functions.c src/csapp.c $(SERVER_PATH_PREFIX)sysinfo.c src/protobuf-c.c src/monitor.pb-c.c $(LIB)
 
-monitorc: $(CLIENT_PATH_PREFIX)monitorc.c csapp.o
-	gcc -o monitorc $(CLIENT_PAHT_PREFIX)monitorc.c csapp.o $(LIB)
+monitorc: src/client/monitorc.c csapp.o src/protobuf-c.c src/monitor.pb-c.c
+	gcc -o monitorc src/client/monitorc.c src/protobuf-c.c src/monitor.pb-c.c csapp.o $(LIB)
 
 csapp.o: src/csapp.c
 	gcc $(CFLAGS) -c src/csapp.c
--- src/client/monitorc.c
+++ src/client/monitorc.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "../include/csapp.h"
-#include "../include/help_message.h"
-#include "../include/monitor.pb-c.h"
+#include "../../include/csapp.h"
+#include "../../include/help_message.h"
+#include "../../include/monitor.pb-c.h"
+
+SystemInfo *static_info(int connfd, void *buffer);
+PerformanceInfo *variable_info(int connfd, void *buffer);
+void show_info_system(SystemInfo *sys);
+void show_info_performance(PerformanceInfo *per);
 
 int main(int argc, char **argv){
 
@@ -11,22 +16,20 @@ int main(int argc, char **argv){
     char *port, *host, buf[MAXLINE];
     rio_t rio;
     void *bufferSys, *bufferPer;
-    SystemInfo sysInfo;
-    PerformanceInfo performace;
+    SystemInfo *sysInfo ;
+    PerformanceInfo *perInfo ;
 
-    switch(c = getopt(argc, argv, "h")){
-	case 'h':
-	    flagh = 1;
-	    break;
-	case '?':
-	    if(argc == 3){
-		host = argv[1];
-		port = argv[2];
-	    }else{
-		host = "127.0.0.1";
-		port = "8000";
-	    }
-	    break;
+    if(argc == 2 && argv[1] == "-h"){
+	flagh = 1;
+    }else if(argc == 3){
+	host = argv[1];
+	port = argv[2];
+    }else if(argc == 1){
+	host = "127.0.0.1";
+	port = "8000";
+    }else{
+	printf("%s\n",HELP_MESSAGE);
+	exit(0);
     }
 
     if(flagh > 0){
@@ -35,56 +38,56 @@ int main(int argc, char **argv){
     }
     clientfd = Open_clientfd(host, port);
 
-    SystemInfo sysInfo = static_info(clientfd,bufferSys);
-    show_info_system(&sysInfo);
-    system_info__free_unpack(&sysInfo, &bufferSys);
+    sysInfo = static_info(clientfd,bufferSys);
+    show_info_system(sysInfo);
+    system_info__free_unpacked(sysInfo, bufferSys);
     
     setbuf(stdin, NULL);
-    while(Fget(buf,MAXLINE,stdin) != NULL){
+    while(Fgets(buf,MAXLINE,stdin) != NULL){
 	if(buf == "q"){
 	    exit(0);
 	}
-	PerformanceInfo perInfo = variable_info(clientfd,bufferPer);
-	show_info_performance(&perInfo);
-	system_info__free_unpack(&perInfo, &bufferPer);	
+	perInfo = variable_info(clientfd,bufferPer);
+	show_info_performance(perInfo);
+	performance_info__free_unpacked(perInfo, bufferPer);	
     }
-    Closed(clientfd);
+    Close(clientfd);
     exit(0);
 }
 
-SystemInfo static_info(int connfd, void *buffer){
+SystemInfo *static_info(int connfd, void *buffer){
     short size;
-    uint8_t valor;
+    void *valloc;
 
     recv(connfd, &size, 2, MSG_NOSIGNAL);
     buffer = malloc(size);
     recv(connfd, &buffer, size, MSG_NOSIGNAL);
-    SystemInfo sistema = system_infor__unpack(&buffer, size, valor);
+    SystemInfo *sistema = system_info__unpack(buffer, size, valloc);
     return sistema;
 }
 
-PerformanceInfo variable_info(int connfd, void *buffer ){
+PerformanceInfo *variable_info(int connfd, void *buffer ){
     short size;
-    uint8_t valor;
+    void *valloc;
 
     recv(connfd, &size, 2, MSG_NOSIGNAL);
     buffer = malloc(size);
     recv(connfd, &buffer, size, MSG_NOSIGNAL);
-    PerformanceInfo performance = performace_info__unpack(&buffer, size, valor);
+    PerformanceInfo *performance = performance_info__unpack(buffer, size, valloc);
     return performance;
 }
 
 void show_info_system(SystemInfo *sys){
-    printf("Nombre del sistema: %s\n",sys.system_name);
-    printf("Version: %s\n",sys.version);
-    printf("Numero de procesadores: %d\n",sys.num_processors);
-    printf("Memoria total: %d\n",sys.mem_total);
+    printf("Nombre del sistema: %s\n",sys->system_name);
+    printf("Version: %s\n",sys->version);
+    printf("Numero de procesadores: %d\n",sys->num_processors);
+    printf("Memoria total: %d\n",sys->mem_total);
 }
 
 void show_info_performance(PerformanceInfo *per){
-    printf("Uptime: %f\n",per.uptime);
-    printf("Porcentaje de uso: %d\n",per.processor_usage );
-    printf("Memoria libre: %d\n",per.mem_free);
-    printf("Numero de procesos: %d\n",per.num_process);
-    printf("Numero de procesos en ejecucion: %d\n",per.num_process_running);
+    printf("Uptime: %f\n",per->uptime);
+    printf("Porcentaje de uso: %ls\n",per->processor_usage );
+    printf("Memoria libre: %d\n",per->mem_free);
+    printf("Numero de procesos: %d\n",per->num_process);
+    printf("Numero de procesos en ejecucion: %d\n",per->num_process_running);
 }
